a376a91c087489c7efe3d1c23eba1a329c939dbe
unlastingstar <a11790783629@qq.com>
2025-02-28 17:18:52 +0800

    Add Binary Tree Recursion Version

--- data-structure.c
+++ data-structure.c
@@ -654,3 +654,116 @@ ElementType RearAndEject(Deque D)
 }
 
 #endif
+
+#ifndef _Tree_H
+
+SearchTree NewNode(ElementType X, SearchTree Left, SearchTree Right)
+{
+    SearchTree T = (SearchTree)Malloc(sizeof(struct TreeNode));
+    T->Element = X;
+    T->Left = Left;
+    T->Right = Right;
+    return T;
+}
+
+void DeleteNode(SearchTree T)
+{
+    Free(T);
+}
+
+SearchTree MakeEmpty(SearchTree T)
+{
+    if (T != NULL)
+    {
+        MakeEmpty(T->Left);
+        MakeEmpty(T->Right);
+        DeleteNode(T);
+    }
+    return NULL;
+}
+
+Position Find(ElementType X, SearchTree T)
+{
+    if (T == NULL)
+        return NULL;
+    if (X < T->Element)
+        return Find(X, T->Left);
+    else if (X > T->Element)
+        return Find(X, T->Right);
+    else
+        return T;
+}
+
+Position FindMin(SearchTree T)
+{
+    if (T == NULL)
+        return NULL;
+    else if (T->Left == NULL)
+        return T;
+    else
+        return FindMin(T->Left);
+}
+
+// Position FindMax(SearchTree T)
+// {
+//     if (T != NULL)
+//         while (T->Right != NULL)
+//             T = T->Right;
+//     return T;
+// }
+
+Position FindMax(SearchTree T)
+{
+    if (T == NULL)
+        return NULL;
+    else if (T->Right == NULL)
+        return T;
+    else
+        return FindMax(T->Right);
+}
+
+SearchTree Insert(ElementType X, SearchTree T)
+{
+    if (T == NULL)
+        T = NewNode(X, NULL, NULL);
+    else if (X < T->Element)
+        T->Left = Insert(X, T->Left);
+    else if (X > T->Element)
+        T->Right = Insert(X, T->Right);
+    return T;
+}
+
+SearchTree Delete(ElementType X, SearchTree T)
+{
+    Position TmpCell;
+
+    if (T == NULL)
+        runtime_error("Element not found");
+    else if (X < T->Element)
+        T->Left = Delete(X, T->Left);
+    else if (X > T->Element)
+        T->Right = Delete(X, T->Right);
+    else if (T->Left && T->Right)
+    {
+        TmpCell = FindMin(T->Right);
+        T->Element = TmpCell->Element;
+        T->Right = Delete(T->Element, T->Right);
+    }
+    else
+    {
+        TmpCell = T;
+        if (T->Left == NULL)
+            T = T->Right;
+        else if (T->Right == NULL)
+            T = T->Left;
+        DeleteNode(TmpCell);
+    }
+    return T;
+}
+
+ElementType Retrieve(Position P)
+{
+    return P->Element;
+}
+
+#endif
